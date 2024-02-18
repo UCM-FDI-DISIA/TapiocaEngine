5c203f1804334bf71c2ce1dbe2156ec29a609ff4
Laura Wang Qiu <yiwang03@ucm.es>
2024-02-18 14:42:10 +0100

    Create cleanBullet.bat

---
+++ Dependencies/Scripts/cleanBullet.bat
@@ -0,0 +1,15 @@
+@echo off
+
+rem Este .bat se encuentra en el directorio Dependencies/Scripts
+
+rem Variable de directorio
+set BULLET_BUILD_DIR=..\Bullet\build\
+
+echo Borrando build de Bullet
+if exist %BULLET_BUILD_DIR% (
+    rmdir /s /q %BULLET_BUILD_DIR%
+    echo Borrada build de Bullet.
+)
+else (
+    echo No se ha encontrado directorio build en Bullet\build. No se realiza nada.
+)
