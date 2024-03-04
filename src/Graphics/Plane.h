652ae378f18d04c6935956600a75b8114f65286c
Jjianuo <Jwen@ucm.es>
2024-03-04 01:21:16 +0100

    Comentarios de  wrapper de plano y constructoras

--- src/Graphics/Plane.h
+++ src/Graphics/Plane.h
@@ -15,20 +15,31 @@ class Plane : public RenderObject{
 protected:
     Ogre::Plane* mPlane;
 
-    Plane();
-    Plane(const Ogre::Vector3& rkNormal, Ogre::Real fConstant);
-    Plane(Ogre::Real a, Ogre::Real b, Ogre::Real c, Ogre::Real _d);
+    //Construye un plano con todo inicializado a 0
+    Plane(Ogre::SceneManager* scnMgr, Node* node);
+    //Construye el plano a partir de la normal rkNormal y la mueve una distancia fConstant en la normal 
+    Plane(Ogre::SceneManager* scnMgr, Node* node, const Vector3& rkNormal, float fConstant);
+    //Construye un plano usando 4 constantes
+    Plane(Ogre::SceneManager* scnMgr, Node* node, float a, float b, float c, float _d);
 
 public:
+    virtual ~Plane() { if(mPlane != nullptr) delete mPlane; };
+    //Devuelve la normal que forma el plano
     Vector3 getNormal() const;
+    //Devuelve la distancia desde el plano al origen
     float getD() const;
-
+    //Devuelve distancia desde un punto al plano
     float getDistance(const Vector3& rkPoint) const;
+    //Redefine el plano a partir de 3 puntos
     void redefine(const Vector3& p0, const Vector3& p1, const Vector3& p2);
+    //Redefine el plano a partir de una normal y un punto
     void redefine(const Vector3& rkNormal, const Vector3& rkPoint);
+    //Proyecta un vector sobre el plano
     Vector3 projectVector(const Vector3& v) const;
+    //Normaliza la normal y la distancia d del plano. Devuelve la longitud de la normal antes de ser normalizada
     float normalise(void);
-    Plane operator-() const;
+
+    //Comparadores del plano
     bool operator==(const Plane& rhs) const;
     bool operator!=(const Plane& rhs) const;
 
