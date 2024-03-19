bbec363b7f2ee926300f4bfd4fc2e7782ff554a3
NanxiQin <nanxiqin@ucm.es>
2024-03-19 10:53:18 +0100

    prueba2

--- src/Core/Components/Transform.cpp
+++ src/Core/Components/Transform.cpp
@@ -201,6 +201,9 @@ Vector3 Transform::forward() {
 
     // Rotar el vector forward utilizando el cuaternion combinado
     Vector3 v = combinedRotation.rotatePoint(f);
+    std::cout << combinedRotation.euler().x << " " << combinedRotation.euler().y << " " << combinedRotation.euler().z
+              << "\n";
+
     std::cout << v.x << " " << v.y << " " << v.z << "\n";
 
     v.normalize();
