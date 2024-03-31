e3f97dc480579f5190c4088c5d6cfbbc5f5c3fd0
maria <marsache@ucm.es>
2024-04-01 01:20:58 +0200

    Componente SpriteRenderer

---
+++ src/Graphics/Components/SpriteRenderer.cpp
@@ -0,0 +1,39 @@
+#include "SpriteRenderer.h"
+#include "../GraphicsManager.h"
+#include "Components/Transform.h"
+#include "Structure/GameObject.h"
+#include "../RenderNode.h"
+#include "../Billboard.h"
+
+namespace Tapioca {
+SpriteRenderer::SpriteRenderer() : billboard(nullptr), node(nullptr), transform(nullptr), billboardName() { }
+SpriteRenderer ::~SpriteRenderer() { delete node; }
+
+bool SpriteRenderer::initComponent(const CompMap& variables) {
+    // No se ha podido establecer o no hay nombre para el billboard
+    if (!setValueFromMap(billboardName, "billboardName", variables) || billboardName == "") {
+#ifdef _DEBUG
+        std::cerr << "Error: SpriteRenderer: no se pudo inicializar el nombre del billboard.\n";
+#endif
+        return false;
+    }
+
+    bool colorSet = setValueFromMap(color.x, "colorX", variables) && setValueFromMap(color.y, "colorY", variables) &&
+        setValueFromMap(color.z, "colorZ", variables) && setValueFromMap(color.w, "colorW", variables);
+    if (!colorSet) {
+#ifdef _DEBUG
+            std::cerr << "No se ha definido un color para el billboard. Se usará un color por defecto.\n";
+#endif
+            color = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+        }
+    }
+
+    void SpriteRenderer::start() {
+        GameObject* gameobject = getObject();
+        transform = gameobject->getComponent<Transform>();
+        GraphicsManager* g = GraphicsManager::instance();
+        node = g->createNode();
+        billboard = g->createBillboard(node, billboardName, transform->getGlobalPosition(), color);
+    }
+    void SpriteRenderer::handleEvent(std::string const& id, void* info) { }
+}
---
+++ src/Graphics/Components/SpriteRenderer.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Structure/Component.h"
+#include "Utilities/Vector4.h"
+
+namespace Tapioca {
+class Billboard;
+class RenderNode;
+class Transform;
+
+class TAPIOCA_API SpriteRenderer : public Component {
+private:
+    RenderNode* node;
+    Billboard* billboard;
+    Transform* transform;
+    Vector4 color;
+
+#ifdef _MSC_VER
+#pragma warning(default : 4251)
+#endif
+    std::string billboardName;
+#ifdef _MSC_VER
+#pragma warning(default : 4251)
+#endif
+
+public:
+    COMPONENT_ID("SpriteRenderer");
+    SpriteRenderer();
+    ~SpriteRenderer();
+
+    bool initComponent(const CompMap& variables) override;
+    void start() override;
+    void handleEvent(std::string const& id, void* info) override;
+};
+}
