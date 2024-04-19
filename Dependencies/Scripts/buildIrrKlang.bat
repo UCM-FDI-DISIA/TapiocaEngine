38de471b2695195d4bc83f1385eff4da446a5a85
likang03 <likangwu@ucm.es>
2024-04-19 17:38:54 +0200

    buildIrrKlang

--- Dependencies/Scripts/buildIrrKlang.bat
+++ Dependencies/Scripts/buildIrrKlang.bat
@@ -1,3 +1,5 @@
 set BAT_DIR=%~dp0
 set BIN_DIR=%BAT_DIR%..\..\bin
-XCOPY %BAT_DIR%..\IrrKlang\dlls\*.dll %BIN_DIR% /Y
+XCOPY %BAT_DIR%..\IrrKlang\dlls\irrKlang.dll %BIN_DIR% /Y
+XCOPY %BAT_DIR%..\IrrKlang\dlls\ikpMP3.dll %BIN_DIR% /Y
+XCOPY %BAT_DIR%..\IrrKlang\dlls\ikpFlac.dll %BIN_DIR% /Y
