eca5ec1f9cc4c80ee943a4edfff22795552eb6f4
Rocio Sanchez <rocisa08@ucm.es>
2024-04-26 10:51:45 +0200

    Check de multiples instancias del mismo sonido

--- src/Sound/Sound.cpp
+++ src/Sound/Sound.cpp
@@ -11,8 +11,15 @@ Sound::Sound(std::string source) {
     //is3D = load3D; ira al audio source
 
     //irrKlang se asegura de solo cargar los arvhivos con un mismo nombre 1 sola vez
+
     mysource = Tapioca::SoundManager::instance()->soundEngine->addSoundSourceFromFile(source.c_str(),
-                                                                                      irrklang::ESM_AUTO_DETECT, true);
+      
+                                                                                irrklang::ESM_AUTO_DETECT, true);
+    
+    //si el sonido ya se ha cargado
+    if (mysource == nullptr) {
+        mysource = Tapioca::SoundManager::instance()->soundEngine->getSoundSource(source.c_str());
+    }
     length = mysource->getPlayLength();
 }
 }
